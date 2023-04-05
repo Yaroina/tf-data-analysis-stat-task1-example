d467419f2d347dba1d83488fee22e6e678e53d36
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-05 22:50:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,5 @@ import numpy as np
 chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean()/7 # Ваш ответ
+    t = 75
+    return (2 / t**2) * np.mean(x)
